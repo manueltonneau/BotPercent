7f8ba63b687e2ed17e14bfa668b17faa8e4f95d9
Manuel <manuel.tonneau@mailbox.org>
2023-07-09 10:56:23 +0200

    add possibility to run full model

--- bot_predict.py
+++ bot_predict.py
@@ -15,7 +15,7 @@ def get_env_var(varname, default):
         print(varname, ':', var, '(Default)')
     return var
 
-def detect(user):
+def detect(user, complete=False):
     #TODO load tweets
     timeline = []
 
@@ -73,22 +73,71 @@ def detect(user):
         entropy
     ]
     # print(len(single_feature))
+    if not complete:
+        single_feature = np.array(single_feature).reshape(1, -1)
+        RF_pred = RF.predict_proba(single_feature)
+        return RF_pred
+    else:
+        single_feature = np.array(single_feature).reshape(1, -1)
+        t5_des = uf.get_des_embedding(user, t5_extract, 512, single)
+        t5_tweet = uf.get_tweets_embedding(timeline, t5_extract, 512)
+        roberta_des = uf.get_des_embedding(user, roberta_extract, 768, single)
+        roberta_tweet = uf.get_tweets_embedding(timeline, roberta_extract, 768)
 
-    single_feature = np.array(single_feature).reshape(1,-1)
-#     t5_des = uf.get_des_embedding(user, t5_extract, 512, single)
-#     t5_tweet = uf.get_tweets_embedding(timeline, t5_extract, 512)
-#     roberta_des = uf.get_des_embedding(user, roberta_extract, 768, single)
-#     roberta_tweet = uf.get_tweets_embedding(timeline, roberta_extract, 768)
+        num = uf.get_num_feat(user, single)
+        cat = uf.get_cat_feat(user, single)
 
-    num = uf.get_num_feat(user, single)
-    cat = uf.get_cat_feat(user, single)
+        RF_pred = RF.predict_proba(single_feature)
+        Ada_pred = Adaboost.predict_proba(single_feature)
 
-    RF_pred = RF.predict_proba(single_feature)
-#     Ada_pred = Adaboost.predict_proba(single_feature)
-    return RF_pred
+        hgt_pred = mlp_hgt(num, cat, roberta_tweet, roberta_des)
+        simplehgn_pred = mlp_simplehgn(num, cat, roberta_tweet, roberta_des)
+        rgcn_pred = mlp_rgcn(num, cat, roberta_tweet, roberta_des)
+        rgt_pred = mlp_rgt(num, cat, roberta_tweet, roberta_des)
+
+        roberta_pred = mlp_roberta(roberta_tweet, roberta_des)
+        t5_pred = mlp_t5(t5_tweet, t5_des)
+
+        # calibration
+        hgt_pred = torch.softmax(hgt_pred / 1.828, dim=-1)  #
+        simplehgn_pred = torch.softmax(simplehgn_pred / 1.818, dim=-1)  #
+        rgt_pred = torch.softmax(rgt_pred / 1.826, dim=-1)  #
+        rgcn_pred = torch.softmax(rgcn_pred / 1.827, dim=-1)  #
+
+        roberta_pred = torch.softmax(roberta_pred / 1.560, dim=-1)  #
+        t5_pred = torch.softmax(t5_pred / 1.552, dim=-1)
+
+        RF_pred = torch.softmax(torch.from_numpy(RF_pred) / 1.415, dim=-1)
+        Ada_pred = torch.softmax(torch.from_numpy(Ada_pred) / 1.498, dim=-1)
+
+        pred_stack = torch.stack([
+            RF_pred,
+            Ada_pred,
+            hgt_pred.detach().cpu(),
+            simplehgn_pred.detach().cpu(),
+            rgt_pred.detach().cpu(),
+            rgcn_pred.detach().cpu(),
+            roberta_pred.detach().cpu(),
+            t5_pred.detach().cpu()
+        ], dim=-1)
+
+        weight = torch.tensor([[1.0990, 1.0991, 0.9009, 0.9008, 0.9008, 0.9008, 0.8996, 0.9006]]).t().double()
+        pred_all = torch.matmul(pred_stack, weight).squeeze(-1)
+        pred_binary = torch.argmax(pred_all, dim=1)
+
+        pred_all = pred_all / pred_all.sum()
+        return pred_all[:, 1].item()
+        # print('-' * 100)
+        # print("pred_score: \t human: {}, bot: {}".format(pred_all[:, 0].item(), pred_all[:, 1].item()))
+        # if pred_binary == 0:
+        #     print('Human!')
+        # else:
+        #     print("Bot!")
+        # print('-' * 100)
 
 if __name__ == '__main__':
     user_set = 'ethnic_hate_users'
+    complete=False
     path_data = os.path.join('/scratch/mt4493/bot_detection/data/user_profiles', user_set)
     input_files_list = list(Path(path_data).glob('*.parquet'))
     # define env var
@@ -104,6 +153,61 @@ if __name__ == '__main__':
     with open("./checkpoint/RandomForest.pkl", 'rb') as f:
         RF = pickle.load(f)
     print('loaded RF')
+    if complete:
+        with open("./checkpoint/Adaboost.pkl", 'rb') as f:
+            Adaboost = pickle.load(f)
+
+        mlp_hgt = MLP(5, 3, 768, 768, 1024, 0.3)
+        hgt_state = torch.load('./checkpoint/HGT.pt', map_location=args.device)
+        mlp_hgt.load_state_dict(hgt_state)
+
+        mlp_simplehgn = MLP(5, 3, 768, 768, 1024, 0.3)
+        simplehgn_state = torch.load('./checkpoint/SimpleHGN.pt', map_location=args.device)
+        mlp_simplehgn.load_state_dict(simplehgn_state)
+
+        mlp_rgt = MLP(5, 3, 768, 768, 1024, 0.3)
+        RGT_state = torch.load('./checkpoint/RGT.pt', map_location=args.device)
+        mlp_rgt.load_state_dict(RGT_state)
+
+        mlp_rgcn = MLP(5, 3, 768, 768, 1024, 0.3)
+        rgcn_state = torch.load('./checkpoint/RGCN.pt', map_location=args.device)
+        mlp_rgcn.load_state_dict(rgcn_state)
+
+        mlp_roberta = MLP_text(768, 768, 128, 0.3)
+        roberta_state = torch.load('./checkpoint/text-RoBERTa.pt', map_location=args.device)
+        mlp_roberta.load_state_dict(roberta_state)
+
+        mlp_t5 = MLP_text(512, 512, 128, 0.3)
+        t5_state = torch.load('./checkpoint/text-T5.pt', map_location=args.device)
+        mlp_t5.load_state_dict(t5_state)
+
+        mlp_hgt.eval()
+        mlp_simplehgn.eval()
+        mlp_rgt.eval()
+        mlp_rgcn.eval()
+        mlp_t5.eval()
+        mlp_roberta.eval()
+
+        pred_all = []
+        roberta_extract = pipeline('feature-extraction',
+                                   model='roberta-base',
+                                   tokenizer='roberta-base',
+                                   device=args.device,
+                                   padding=True,
+                                   truncation=True,
+                                   max_length=50,
+                                   add_special_tokens=True)
+
+        tokenizer = T5Tokenizer.from_pretrained('t5-small')
+        model = T5EncoderModel.from_pretrained('t5-small')
+        t5_extract = pipeline('feature-extraction',
+                              model=model,
+                              tokenizer=tokenizer,
+                              device=args.device,
+                              padding=True,
+                              truncation=True,
+                              max_length=50,
+                              add_special_tokens=True)
     output_path = '/scratch/mt4493/bot_detection/results'
     if not os.path.exists(output_path):
         os.makedirs(output_path, exist_ok=True)
